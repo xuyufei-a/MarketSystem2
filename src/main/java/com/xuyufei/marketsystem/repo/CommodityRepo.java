0cba0e14f093cf8bdddfd765f733646033e9e7ed
xuyufei <2412453584@qq.com>
2022-12-04 01:34:29 +0800

    Finish the CommodityRepo Class, which is similar to the UserRepo.

--- src/main/java/com/xuyufei/marketsystem/repo/CommodityRepo.java
+++ src/main/java/com/xuyufei/marketsystem/repo/CommodityRepo.java
@@ -1,4 +1,87 @@
 package com.xuyufei.marketsystem.repo;
 
-public class CommodityRepo {
+import com.xuyufei.marketsystem.entity.Commodity;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class CommodityRepo extends Repo implements Closeable {
+    private static Connection connection;
+
+    CommodityRepo() {
+        try {
+            connection = DriverManager.getConnection(URL);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    public void close() throws IOException {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Iterable<Integer> getIDsByName(String name) {
+        try(Statement statement = connection.createStatement();) {
+            String sql = "select * from commodity where name=" + name;
+            var resultSet = statement.executeQuery(sql);
+
+            var ret = new ArrayList<Integer>();
+            while(resultSet.next()) {
+                ret.add(resultSet.getInt("id"));
+            }
+            return ret;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Iterable<Integer> getIDsByEntityName(Commodity com) {
+        return getIDsByName(com.getName());
+    }
+
+    // TODO: 2022/12/4
+    // In the function, the codes to import a image may be incorrect
+    public Commodity getEntityByID(int id) {
+        try(Statement statement = connection.createStatement();) {
+            String sql = "select * from commodity where id=" + id;
+            var resultSet = statement.executeQuery(sql);
+            if(resultSet.next() == false)   return null;
+
+            var id2 = resultSet.getInt("id");
+            var name = resultSet.getString("name");
+            var imagePath = resultSet.getString("imagePath");
+            var text = resultSet.getString("text");
+            var price = resultSet.getInt("price");
+            var owner = resultSet.getInt("owner");
+            return new Commodity(id2, name, imagePath, text, price, owner);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Iterable<Integer> getIDsByOwnerID(int ownerID) {
+        try(Statement statement = connection.createStatement();) {
+            String sql = "select * from commodity where owner=" + ownerID;
+            var resultSet = statement.executeQuery(sql);
+
+            var ret = new ArrayList<Integer>();
+            while(resultSet.next()) {
+                ret.add(resultSet.getInt("owner"));
+            }
+            return ret;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
